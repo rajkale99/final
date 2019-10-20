ef21b474bfce0ad632a351e6066bee21a648364d
Rajkale99 <kaleraj.rk@gmail.com>
2019-10-20 13:32:53 +0000

    Checking for android q

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 AospExtended ROM
+# Copyright (C) 2018 The LegionOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
